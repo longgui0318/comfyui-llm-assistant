80c4df96ab0881249c76d3ffb9374820915cd232
gubei <>
2024-02-27 16:19:10 +0800

    add llm node

---
+++ AssistantNodes.py
@@ -0,0 +1,303 @@
+from .utils import *
+
+
+class GenerateStableDiffsutionPromptLLM:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        data = load_config()
+        template_system = ""
+        template_user = ""
+        models = []
+
+        if data:
+            _prompt = data["default_generate_stable_diffsution_prompt"]
+            if _prompt:
+                _system = _prompt["system"]
+                _user = _prompt["user"]
+                if _system:
+                    template_system = _system
+                if _user:
+                    template_user = _user
+            _models = data["models"]
+            if isinstance(_models, list) and len(_models) > 0:
+                models = _models
+            default_model = data["default_model"]
+            if default_model and len(_models) > 0:
+                # 找到default_model的index
+                default_model_index = models.index(default_model)
+                if default_model_index > 0:
+                    models.pop(default_model_index)
+                    models.insert(0, default_model)
+        return {
+            "required": {
+                "prompt": ("STRING", {
+                    "multiline": True,
+                    "default": ""
+                }),
+                "response_pattern": ("STRING", {
+                    "multiline": False,
+                    "default": None
+                }),
+                "template_system": ("STRING", {
+                    "multiline": True,
+                    "default": template_system,
+                    "display": "textarea",
+                }),
+                "template_user": ("STRING", {
+                    "multiline": True,
+                    "default": template_user,
+                    "display": "textarea"
+                }),
+                "temperature": ("FLOAT", {
+                    "default": 1.0,
+                    "min": 0.0,
+                    "max": 1.0,
+                    "step": 0.01,
+                    "round": 0.01,
+                    "display": "number"
+                }),
+
+                "max_tokens": ("INT", {
+                    "default": 300,
+                    "min": -1,
+                    "max": 2048,
+                    "display": "number"
+                }),
+                "model_name": (models, {
+                    "default": default_model,
+                    "display": "select"
+                })
+            }
+        }
+
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("stable diffsution prompt",)
+
+    FUNCTION = "generateStableDiffsutionPrompt"
+
+    CATEGORY = "LLM"
+
+    def generateStableDiffsutionPrompt(self, prompt, response_pattern, template_system, template_user, temperature, max_tokens, model_name):
+        config = load_config()
+        openai_config = config["openai"]
+        if openai_config is None:
+            return (prompt,)
+        api_base = openai_config["api_base"]
+        api_key = openai_config["api_key"]
+        if not (is_valid_string(api_base) and is_valid_string(api_key)):
+            return (prompt,)
+        _prompt = prompt
+        if is_valid_string(template_user):
+            _prompt = template_user.format(prompt)
+        response = get_completion(_prompt, response_pattern, api_base, api_key, temperature, template_system,
+                                  max_tokens, model_name)
+        return (response,)
+
+
+class TranslateTextLLM:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        data = load_config()
+        template_system = ""
+        template_user = ""
+        models = []
+
+        if data:
+            _prompt = data["default_translate_prompt"]
+            if _prompt:
+                _system = _prompt["system"]
+                _user = _prompt["user"]
+                if _system:
+                    template_system = _system
+                if _user:
+                    template_user = _user
+            _models = data["models"]
+            if isinstance(_models, list) and len(_models) > 0:
+                models = _models
+            default_model = data["default_model"]
+            if default_model and len(_models) > 0:
+                # 找到default_model的index
+                default_model_index = models.index(default_model)
+                if default_model_index > 0:
+                    models.pop(default_model_index)
+                    models.insert(0, default_model)
+        return {
+            "required": {
+                "prompt": ("STRING", {
+                    "multiline": True,
+                    "default": ""
+                }),
+                "response_pattern": ("STRING", {
+                    "multiline": False,
+                    "default": None
+                }),
+                "template_system": ("STRING", {
+                    "multiline": True,
+                    "default": template_system,
+                    "display": "textarea",
+                }),
+                "template_user": ("STRING", {
+                    "multiline": True,
+                    "default": template_user,
+                    "display": "textarea"
+                }),
+                "temperature": ("FLOAT", {
+                    "default": 1.0,
+                    "min": 0.0,
+                    "max": 1.0,
+                    "step": 0.01,
+                    "round": 0.01,
+                    "display": "number"
+                }),
+
+                "max_tokens": ("INT", {
+                    "default": 300,
+                    "min": -1,
+                    "max": 2048,
+                    "display": "number"
+                }),
+                "model_name": (models, {
+                    "default": default_model,
+                    "display": "select"
+                })
+            }
+        }
+
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("text",)
+
+    FUNCTION = "translateText"
+
+    CATEGORY = "LLM"
+
+    def translateText(self, prompt, response_pattern, template_system, template_user, temperature, max_tokens, model_name):
+        config = load_config()
+        openai_config = config["openai"]
+        if openai_config is None:
+            return (prompt,)
+        api_base = openai_config["api_base"]
+        api_key = openai_config["api_key"]
+        if not (is_valid_string(api_base) and is_valid_string(api_key)):
+            return (prompt,)
+        _prompt = prompt
+        if is_valid_string(template_user):
+            _prompt = template_user.format(prompt)
+        response = get_completion(_prompt, response_pattern, api_base, api_key, temperature, template_system,
+                                  max_tokens, model_name)
+        return (response,)
+
+class ChatWithLLM:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        data = load_config()
+        template_system = ""
+        template_user = ""
+        models = []
+
+        if data:
+            _prompt = data["default_chat_prompt"]
+            if _prompt:
+                _system = _prompt["system"]
+                _user = _prompt["user"]
+                if _system:
+                    template_system = _system
+                if _user:
+                    template_user = _user
+            _models = data["models"]
+            if isinstance(_models, list) and len(_models) > 0:
+                models = _models
+            default_model = data["default_model"]
+            if default_model and len(_models) > 0:
+                # 找到default_model的index
+                default_model_index = models.index(default_model)
+                if default_model_index > 0:
+                    models.pop(default_model_index)
+                    models.insert(0, default_model)
+        return {
+            "required": {
+                "prompt": ("STRING", {
+                    "multiline": True,
+                    "default": ""
+                }),
+                "response_pattern": ("STRING", {
+                    "multiline": False,
+                    "default": None
+                }),
+                "template_system": ("STRING", {
+                    "multiline": True,
+                    "default": template_system,
+                    "display": "textarea",
+                    # 增加高度设置
+                    "height": 100
+                }),
+                "template_user": ("STRING", {
+                    "multiline": True,
+                    "default": template_user,
+                    "display": "textarea"
+                }),
+                "temperature": ("FLOAT", {
+                    "default": 1.0,
+                    "min": 0.0,
+                    "max": 1.0,
+                    "step": 0.01,
+                    "round": 0.01,
+                    "display": "number"
+                }),
+
+                "max_tokens": ("INT", {
+                    "default": 300,
+                    "min": -1,
+                    "max": 2048,
+                    "display": "number"
+                }),
+                "model_name": (models, {
+                    "default": default_model,
+                    "display": "select"
+                })
+            }
+        }
+
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("text",)
+
+    FUNCTION = "chatLLM"
+
+    CATEGORY = "LLM"
+
+    def chatLLM(self, prompt, response_pattern, template_system, template_user, temperature, max_tokens, model_name):
+        config = load_config()
+        openai_config = config["openai"]
+        if openai_config is None:
+            return (prompt,)
+        api_base = openai_config["api_base"]
+        api_key = openai_config["api_key"]
+        if not (is_valid_string(api_base) and is_valid_string(api_key)):
+            return (prompt,)
+        _prompt = prompt
+        if is_valid_string(template_user):
+            _prompt = template_user.format(prompt)
+        response = get_completion(_prompt, response_pattern, api_base, api_key, temperature, template_system,
+                                  max_tokens, model_name)
+        return (response,)
+
+
+NODE_CLASS_MAPPINGS = {
+    "Generate Stable Diffsution Prompt With LLM": GenerateStableDiffsutionPromptLLM,
+    "Translate Text With LLM": TranslateTextLLM,
+    "Chat With LLM": ChatWithLLM,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Generate Stable Diffsution Prompt With LLM": "Generate Stable Diffsution Prompt With LLM",
+    "Translate Text With LLM": "Translate Text With LLM",
+    "Chat With LLM": "Chat With LLM",
+}
---
+++ utils.py
@@ -0,0 +1,53 @@
+from openai import OpenAI
+import re
+import os
+import yaml
+
+
+def is_valid_string(s):
+    return s is not None and s.strip() != ""
+
+
+def get_completion(prompt, response_pattern, api_url, api_key, temperature, sys_prefix, max_tokens, model="local model"):
+    try:
+        client = OpenAI(api_key=api_key, base_url=api_url)
+        messages = [{"role": "system", "content": sys_prefix},
+                    {"role": "user", "content": prompt}]
+
+        response = client.chat.completions.create(
+            model=model,
+            messages=messages,
+            temperature=temperature,
+            max_tokens=max_tokens
+        )
+        response_str = response.choices[0].message.content
+        if response_pattern:
+            try:
+                response_str_t = re.search(
+                    response_pattern, response_str).group(0)
+                if response_str_t:
+                    response_str = response_str_t
+            except:
+                pass
+
+        return response_str
+
+    except Exception as e:
+        error_message = f"Error: {str(e)}"
+        print(error_message)
+        return prompt
+
+
+config_data = None
+
+
+def load_config():
+    global config_data
+    if config_data:
+        return config_data
+    my_path = os.path.dirname(__file__)
+    with open(os.path.join(my_path, "config.yaml"), 'r') as file:
+        # 使用yaml.load()解析YAML文件内容
+        config_data = yaml.load(file, Loader=yaml.FullLoader)
+
+    return config_data
